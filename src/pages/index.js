ddafd254bd508e7dbd1782a9b8f9d2b3dc33194d
Tom Canham <31967070+thomascanham@users.noreply.github.com>
2020-04-09 12:54:51 +0100

    changed the graphql query on the index page to pull different data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,31 +2,37 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>{data.allWordpressWpServices.nodes[0].title}</h1>
-    <div dangerouslySetInnerHTML={{ __html: data.allWordpressWpServices.nodes[0].content }}></div>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout >
-)
+
+const IndexPage = ({ data }) => {
+  const { acf } = data.allWordpressPage.edges[0].node;
+  const { title } = data.allWordpressPage.edges[0].node;
+
+  return (
+    <Layout>
+      <SEO title={title} />
+      <h1>{acf.main_heading}</h1>
+      <p>{acf.welcome_text}</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout >
+  )
+}
 
 
 export const query = graphql`
-  query site {
-    allWordpressWpServices {
-      nodes {
-        content
-        id
-        wordpress_id
-        title
+  query {
+    allWordpressPage(filter: {wordpress_id: {eq: 5}}) {
+      edges {
+        node {
+          id
+          title
+          wordpress_id
+          acf {
+            main_heading
+            welcome_text
+          }
+        }
       }
     }
   }
